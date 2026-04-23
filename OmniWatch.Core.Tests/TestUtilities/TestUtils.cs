5540533e85b60165cfa2f7e34e5bf32e387b716b
Luciano Campos <51826416+jlKampos@users.noreply.github.com>
2026-04-23 11:01:05 +0100

    integration tests for Core

---
+++ OmniWatch.Core.Tests/TestUtilities/TestUtils.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace OmniWatch.Core.Tests.TestUtilities
+{
+    public static class TestUtils
+    {
+        public static void SetPrivateField(object obj, string fieldName, object value)
+        {
+            var field = obj.GetType()
+                .GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+
+            field!.SetValue(obj, value);
+        }
+    }
+}
--- OmniWatch.Core/Helpers/OpenSkyRateLimitTable.cs
+++ OmniWatch.Core/Helpers/OpenSkyRateLimitTable.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OmniWatch.Core.Helpers
+﻿namespace OmniWatch.Core.Helpers
 {
     public static class OpenSkyRateLimitTable
     {
